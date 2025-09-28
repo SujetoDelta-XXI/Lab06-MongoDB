ae84501b36c143275c9692c1db6e11ae58217816
Carlos Asparrin Martin <150694554+SujetoDelta-XXI@users.noreply.github.com>
2025-09-27 21:33:52 -0500

    Codigo final

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
+// app.js
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
-import connectDB from "./src/db/database.js";
 import dotenv from "dotenv";
-dotenv.config(); // carga las variables desde .env
+import connectDB from "./src/db/database.js";
 
-//rutas
+// Rutas
 import homeRoutes from "./src/routes/home.routes.js";
 import postRoutes from "./src/routes/post.routes.js";
 
+// Repositorios para la creación inicial de datos
+import userRepository from "./src/repositories/userRepository.js";
+import postRepository from "./src/repositories/postRepository.js";
+
+dotenv.config();
 const app = express();
+
+// Configuración de rutas y carpetas
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src","views"));
+app.set("views", path.join(__dirname, "src", "views"));
 
-// Middlewares
-app.use(express.urlencoded({ extended: true })); // Para leer datos de formularios
-app.use(express.json()); // Para leer JSON
-app.use(express.static(path.join(__dirname,"src", "public"))); // Archivos estáticos (css, js, imgs)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "src", "public")));
 
-// Rutas
 app.use("/", homeRoutes);
 app.use("/posts", postRoutes);
 
-connectDB(); //Conexión a la base de datos
+// Conexión a la BD
+connectDB().then(async () => {
+  console.log("🔗 MongoDB conectado");
+});
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () =>
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`)
+);
